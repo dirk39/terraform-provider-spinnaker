47c81a1c21ea609ed8b7ed18bc0be03f2d08566c
dirk39 <andreaquin1990@gmail.com>
2020-04-26 16:41:19 +0200

    add test for V2CreatePipelineTemplate method

--- spinnaker/api/v2_template_test.go
+++ spinnaker/api/v2_template_test.go
@@ -15,6 +15,8 @@ func (client mockedAPIClient) createPipelineTemplateV2(values interface{}) (*htt
 	return createPipelineTemplateV2Mock(values)
 }
 
+// TestV2CreatePipelineTemplateReturnErrorFromApi test that
+// V2CreatePipelineTemplate return error from api
 func TestV2CreatePipelineTemplateReturnErrorFromApi(t *testing.T) {
 	createPipelineTemplateV2Mock = func(interface{}) (*http.Response, error) {
 		return nil, errors.New("This is an error")
@@ -28,4 +30,30 @@ func TestV2CreatePipelineTemplateReturnErrorFromApi(t *testing.T) {
 	}
 
 	t.Log("TestV2CreatePipelineTemplateReturnErrorFromApi PASS")
+}
+
+// TestV2CreatePipelineTemplateReturnErrorByStatusCode test that
+// V2CreatePipelineTemplate return error from api if status code is different
+// from accepted once
+func TestV2CreatePipelineTemplateReturnErrorByStatusCode(t *testing.T) {
+	createPipelineTemplateV2Mock = func(interface{}) (*http.Response, error) {
+		response := http.Response{
+			StatusCode: 400,
+		}
+
+		return &response, nil
+	}
+	var mock mockedAPIClient
+
+	err := V2CreatePipelineTemplate(mock, "useless")
+
+	if nil == err {
+		t.Fatal("TestV2CreatePipelineTemplateReturnErrorByStatusCode failed: expecting error, got nil")
+	}
+
+	if err.Error() != "Encountered an error saving template, status code: 400" {
+		t.Fatalf(`TestV2CreatePipelineTemplateReturnErrorByStatusCode failed: expecting "Encountered an error saving template, status code: 400", got %v`, err.Error())
+	}
+
+	t.Log("TestV2CreatePipelineTemplateReturnErrorByStatusCode PASS")
 }
